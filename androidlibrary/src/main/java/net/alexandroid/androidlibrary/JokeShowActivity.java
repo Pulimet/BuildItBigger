97ba4cedc1218c75987c3652b947c5ab8a637d4c
Alexey Korolev <korolev2009@gmail.com>
2016-01-04 08:48:56 +0200

    Working on step 2

--- androidlibrary/src/main/java/net/alexandroid/androidlibrary/JokeShowActivity.java
+++ androidlibrary/src/main/java/net/alexandroid/androidlibrary/JokeShowActivity.java
@@ -1,7 +1,9 @@
 package net.alexandroid.androidlibrary;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
 public class JokeShowActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class JokeShowActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_joke_show);
+
+        Bundle bundle = getIntent().getExtras();
+        if (bundle != null && bundle.containsKey(Intent.EXTRA_TEXT)) {
+            Toast.makeText(this, bundle.getString(Intent.EXTRA_TEXT, getString(R.string.error)), Toast.LENGTH_SHORT).show();
+        }
     }
 }
--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -1,25 +1,69 @@
 package com.udacity.gradle.builditbigger;
 
+import android.content.Context;
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v4.util.Pair;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Toast;
 
+import com.alexandroid.udacity.sample.myApi.MyApi;
 import com.example.Jokes;
+import com.google.api.client.extensions.android.http.AndroidHttp;
+import com.google.api.client.extensions.android.json.AndroidJsonFactory;
+import com.google.api.client.googleapis.services.AbstractGoogleClientRequest;
+import com.google.api.client.googleapis.services.GoogleClientRequestInitializer;
 
-//import com.example.Jokes;
+import net.alexandroid.androidlibrary.JokeShowActivity;
 
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new EndpointsAsyncTask().execute(new Pair<Context, String>(this, "Manfred"));
     }
 
     public void tellJoke(View view) {
         Jokes jokes = new Jokes();
-        Toast.makeText(this, jokes.getJoke(), Toast.LENGTH_SHORT).show();
+        Intent intent =  new Intent(this, JokeShowActivity.class);
+        intent.putExtra(Intent.EXTRA_TEXT, jokes.getJoke());
+        startActivity(intent);
+    }
+
+    class EndpointsAsyncTask extends AsyncTask<Pair<Context, String>, Void, String> {
+        private MyApi myApiService = null;
+        private Context context;
+
+        @Override
+        protected String doInBackground(Pair<Context, String>... params) {
+            if(myApiService == null) {  // Only do this once
+                MyApi.Builder builder = new MyApi.Builder(AndroidHttp.newCompatibleTransport(), new AndroidJsonFactory(), null)
+                        .setRootUrl("https://android-app-backend.appspot.com/_ah/api/");
+            }
+
+            context = params[0].first;
+            String name = params[0].second;
+
+            try {
+                return myApiService.sayHi(name).execute().getData();
+            } catch (IOException e) {
+                return e.getMessage();
+            }
+        }
+                
+        @Override
+        protected void onPostExecute(String result) {
+            Toast.makeText(context, result, Toast.LENGTH_LONG).show();
+        }
     }
 
 }
