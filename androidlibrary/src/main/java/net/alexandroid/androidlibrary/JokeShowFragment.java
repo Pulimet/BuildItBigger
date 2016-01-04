689a1e43fab623091a6208c760692754607ac413
Alexey Korolev <korolev2009@gmail.com>
2016-01-04 06:35:19 +0200

    Working on step 2

---
+++ androidlibrary/src/main/java/net/alexandroid/androidlibrary/JokeShowFragment.java
@@ -0,0 +1,20 @@
+package net.alexandroid.androidlibrary;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class JokeShowFragment extends Fragment {
+
+    public JokeShowFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_joke_show, container, false);
+
+        return root;
+    }
+}
